c21294de8825ff1de53c14ffc5ec282c2c60a401
Tim Holm <tim.holm@nitric.io>
2021-09-13 10:30:52 +1000

    chore: Add index.js and main.

---
+++ node/index.js
@@ -0,0 +1,5 @@
+// base module does not directly export anything
+// modules to be resolved absolutely in this package
+// e.g. require("@nitric/base-sdk/proto/document/v1/document_pb");
+// OR import * as docpb from "@nitric/base-sdk/proto/document/v1/document_pb"
+module.exports = {};
